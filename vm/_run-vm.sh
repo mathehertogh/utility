64cb3a778610416c5fbc180ad1587abd130746f2
mathe <mathe@laptop-mathe>
2025-10-22 14:58:50 +0200

    vm: minor

--- vm/_run-vm.sh
+++ vm/_run-vm.sh
@@ -12,7 +12,7 @@ sudo taskset -c $CPUS \
 		-smp sockets=1,cores=1,threads=2 \
 		-m 8192 \
 		-kernel $NAME.bz \
-		-append "root=/dev/sda ro console=ttyS0 earlyprintk=serial" \
+		-append "root=/dev/sda ro console=ttyS0 earlyprintk=serial net.ifnames=0" \
 		-drive file=$NAME.img,format=raw \
 		-net user,hostfwd=tcp::$PORT-:22 -net nic \
 		-net nic -netdev tap,id=tap0 -device e1000,netdev=tap0 \
