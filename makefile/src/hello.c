5cc813a1dad64fe733bad1358d360051cc818f74
mathe <mathe@laptop-mathe>
2025-10-22 11:00:44 +0200

    makefile

---
+++ makefile/src/hello.c
@@ -0,0 +1,6 @@
+#include "hello.h"
+
+void hello(const char *who)
+{
+	printf("hello %s!\n", who);
+}
---
+++ makefile/src/main.c
@@ -0,0 +1,9 @@
+#include "num.h"
+#include "hello.h"
+
+int main()
+{
+	printf("num = %d\n", NUM);
+	hello("Mathé");
+	return 0;
+}
